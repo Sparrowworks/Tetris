940acf0fc9e9bb89f5414d39d804ac5de1676ff3
SP4R0W <sp4r0wxxx@gmail.com>
2024-06-17 12:20:33 +0200

    Updated visuals; added inputs

--- src/Game/Game.gd
+++ src/Game/Game.gd
@@ -1,5 +1,35 @@
 class_name Game extends Node2D
 
+func pause() -> void:
+	pass
+
+func reset() -> void:
+	pass
+
+func exit() -> void:
+	ComposerGD.ReplaceScene("Game","MainMenu",Global.main)
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("exit"):
+		exit()
+
+	if Input.is_action_just_pressed("pause"):
+		pause()
+
+	if Input.is_action_just_pressed("reset"):
+		reset()
+
+func _on_pause_pressed() -> void:
+	pause()
+
+func _on_reset_pressed() -> void:
+	reset()
 
 func _on_exit_pressed() -> void:
-	ComposerGD.ReplaceScene("Game","MainMenu",Global.main)
+	exit()
+
+
+
+
+
+
--- src/Global/Global.gd
+++ src/Global/Global.gd
@@ -1,3 +1,5 @@
 extends Node
 
 @onready var main: Node = null
+
+const BLOCK_SIZE: int = 90
