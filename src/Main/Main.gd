7b9619cd2c8949c6f409cbfab327ae6297c08982
SP4R0W <sp4r0wxxx@gmail.com>
2024-06-17 11:19:01 +0200

    Added assets; started working on grid

--- src/Main/Main.gd
+++ src/Main/Main.gd
@@ -1,6 +1,13 @@
 extends Node2D
 
 func _ready() -> void:
+	Global.main = self
+
+	ComposerGD.AddScene("Game","res://src/Game/Game.tscn",
+	{
+		"instant_load":true
+	})
+
 	ComposerGD.AddScene("MainMenu","res://src/MainMenu/MainMenu.tscn",
 	{
 		"instant_load":true,
