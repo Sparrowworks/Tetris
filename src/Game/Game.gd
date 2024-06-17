708c0ff1708a8642c6e1f68953f352f429a8c77d
SP4R0W <sp4r0wxxx@gmail.com>
2024-06-17 11:41:43 +0200

    Added UI

--- src/Game/Game.gd
+++ src/Game/Game.gd
@@ -1,1 +1,5 @@
 class_name Game extends Node2D
+
+
+func _on_exit_pressed() -> void:
+	ComposerGD.ReplaceScene("Game","MainMenu",Global.main)
--- src/MainMenu/MainMenu.gd
+++ src/MainMenu/MainMenu.gd
@@ -2,3 +2,7 @@ extends Node2D
 
 func _on_play_pressed() -> void:
 	ComposerGD.ReplaceScene("MainMenu","Game",Global.main)
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
