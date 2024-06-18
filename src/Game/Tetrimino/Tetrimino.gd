bf41a9fe6c6750abaf4aee068758726ee835aa7a
SP4R0W <sp4r0wxxx@gmail.com>
2024-06-18 11:17:13 +0200

    Added collision shapes for blocks

--- src/Game/Tetrimino/Tetrimino.gd
+++ src/Game/Tetrimino/Tetrimino.gd
@@ -10,6 +10,7 @@ class_name Tetrimino extends Node2D
 @onready var block_2: Block = $Block2
 @onready var block_3: Block = $Block3
 @onready var block_4: Block = $Block4
+@onready var collision_shape_2d: CollisionShape2D = %CollisionShape2D
 
 func _process(delta: float) -> void:
 	if Engine.is_editor_hint():
@@ -37,6 +38,8 @@ func draw_new_block() -> void:
 			block_3.animation = "Blue"
 			block_4.animation = "Blue"
 
+			collision_shape_2d.shape = Global.SHAPE_I
+
 			block_1.position = Vector2(45,-45)
 			block_2.position = Vector2(45,135)
 			block_3.position = Vector2(45,45)
@@ -46,6 +49,8 @@ func draw_new_block() -> void:
 			block_2.animation = "Pink"
 			block_3.animation = "Pink"
 			block_4.animation = "Pink"
+
+			collision_shape_2d.shape = Global.SHAPE_J
 
 			block_1.position = Vector2(135,-45)
 			block_2.position = Vector2(135,45)
@@ -57,6 +62,8 @@ func draw_new_block() -> void:
 			block_3.animation = "Orange"
 			block_4.animation = "Orange"
 
+			collision_shape_2d.shape = Global.SHAPE_L
+
 			block_1.position = Vector2(-45,-45)
 			block_2.position = Vector2(135,45)
 			block_3.position = Vector2(45,45)
@@ -66,6 +73,8 @@ func draw_new_block() -> void:
 			block_2.animation = "Yellow"
 			block_3.animation = "Yellow"
 			block_4.animation = "Yellow"
+
+			collision_shape_2d.shape = Global.SHAPE_O
 
 			block_1.position = Vector2(-45,-45)
 			block_2.position = Vector2(45,-45)
@@ -77,6 +86,8 @@ func draw_new_block() -> void:
 			block_3.animation = "Green"
 			block_4.animation = "Green"
 
+			collision_shape_2d.shape = Global.SHAPE_S
+
 			block_1.position = Vector2(135,-45)
 			block_2.position = Vector2(45,-45)
 			block_3.position = Vector2(45,45)
@@ -87,6 +98,8 @@ func draw_new_block() -> void:
 			block_3.animation = "Purple"
 			block_4.animation = "Purple"
 
+			collision_shape_2d.shape = Global.SHAPE_T
+
 			block_1.position = Vector2(135,45)
 			block_2.position = Vector2(45,-45)
 			block_3.position = Vector2(45,45)
@@ -96,6 +109,8 @@ func draw_new_block() -> void:
 			block_2.animation = "Red"
 			block_3.animation = "Red"
 			block_4.animation = "Red"
+
+			collision_shape_2d.shape = Global.SHAPE_Z
 
 			block_1.position = Vector2(135,45)
 			block_2.position = Vector2(45,-45)
--- src/Global/Global.gd
+++ src/Global/Global.gd
@@ -2,4 +2,12 @@ extends Node
 
 @onready var main: Node = null
 
+const SHAPE_I = preload("res://Assets/Shapes/shape_i.tres")
+const SHAPE_J = preload("res://Assets/Shapes/shape_j.tres")
+const SHAPE_L = preload("res://Assets/Shapes/shape_l.tres")
+const SHAPE_O = preload("res://Assets/Shapes/shape_o.tres")
+const SHAPE_S = preload("res://Assets/Shapes/shape_s.tres")
+const SHAPE_T = preload("res://Assets/Shapes/shape_t.tres")
+const SHAPE_Z = preload("res://Assets/Shapes/shape_z.tres")
+
 const BLOCK_SIZE: int = 90
