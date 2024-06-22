748cd4d335ed8002110c88a34df924bd70ee3086
SP4R0W <sp4r0wxxx@gmail.com>
2024-06-22 09:58:30 +0200

    Changed movement system and grid to use Tilemap; correct down movement for now

--- src/Game/Grid/Grid.gd
+++ src/Game/Grid/Grid.gd
@@ -35,6 +35,8 @@ func _ready() -> void:
 	clear_board()
 	generate_new_block()
 
+#region BOARD_FUNCTIONS
+
 func clear_board(fast: bool = true) -> void:
 	if fast:
 		clear()
@@ -42,40 +44,70 @@ func clear_board(fast: bool = true) -> void:
 			for y in range(0,MAX_Y):
 				set_cell(0,Vector2i(x,y),BLOCK_IDS.GREY,ATLAS_COORDS)
 
-func generate_new_block():
-	var block_id: int = randi_range(0,6)
-	var block_resource = BLOCK_FILES[block_id]
-	var block_spawn_coords = block_resource.SpawnCoords
+func generate_new_block() -> void:
+	var block_id: int = 3
+	var block_resource: Block = BLOCK_FILES[block_id]
+	var block_spawn_coords: Array[Vector2i] = block_resource.SpawnCoords
 	var block_color: int = block_resource.id
 
-	for coord in block_spawn_coords:
+	for coord: Vector2i in block_spawn_coords:
 		set_cell(0,coord,block_color,ATLAS_COORDS)
 		active_block_coords.append(coord)
 
 	active_block_id = block_id
 
-func move_down() -> void:
-	var new_coords: Array[Vector2i] = []
-	print(active_block_coords)
+#endregion
 
-	for x in range(0,active_block_coords.size()):
-		var old_coord = active_block_coords[x]
-		var new_coord: Vector2i = Vector2i(old_coord.x,old_coord.y+1)
+#region MOVEMENT
+func move_down() -> void:
+	var updated_coords: Array[Vector2i] = get_new_coords(Vector2i.DOWN)
+
+	if can_move_down(updated_coords):
+		update_coords(updated_coords)
+
+#func move_left() -> void:
+	#update_coords(Vector2i.LEFT)
+#
+#func move_right() -> void:
+	#update_coords(Vector2i.RIGHT)
+
+func get_new_coords(vector: Vector2i) -> Array[Vector2i]:
+	var new_coords: Array[Vector2i] = []
+
+	for i in range(0,active_block_coords.size()):
+		var old_coord: Vector2i = active_block_coords[i]
+		new_coords.append(Vector2i(old_coord.x + vector.x, old_coord.y + vector.y))
+
+	return new_coords
+
+func update_coords(coords: Array[Vector2i]) -> void:
+	for i in range(0,active_block_coords.size()):
+		var old_coord: Vector2i = active_block_coords[i]
+		active_block_coords[i] = coords[i]
 
 		set_cell(0,old_coord,BLOCK_IDS.GREY,ATLAS_COORDS)
-		force_update()
-		set_cell(0,new_coord,active_block_id,ATLAS_COORDS)
-		new_coords.append(new_coord)
 
-	active_block_coords = new_coords
-	print(active_block_coords)
+	for coord in active_block_coords:
+		set_cell(0,coord,active_block_id,ATLAS_COORDS)
+
+#endregion MOVEMENT
+
+#region VERIFY_MOVEMENT
+func can_move_down(coords: Array[Vector2i]) -> bool:
+	for coord: Vector2i in coords:
+		if coord.y > MAX_Y-1:
+			return false
+
+	return true
+
+#endregion
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("down"):
 		move_down()
 
-func pause():
-	pass
+	#if Input.is_action_just_pressed("left"):
+		#move_left()
+	#elif Input.is_action_just_pressed("right"):
+		#move_right()
 
-func unpause():
-	pass
