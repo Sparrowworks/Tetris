6a3eb9441500f8ccfc3b4c2e309909fe27ed0d6e
SP4R0W <sp4r0wxxx@gmail.com>
2025-03-16 17:21:20 +0100

    Updated to 4.4

--- src/MainMenu/MainMenu.gd
+++ src/MainMenu/MainMenu.gd
@@ -1,10 +1,25 @@
 extends Node2D
 
+@onready var version_text: Label = $CanvasLayer/Control/VersionText
+
 func _ready() -> void:
 	Global.main.play_menu_theme()
 
 	if OS.has_feature("web"):
 		$CanvasLayer/Control/Buttons/Exit.hide()
+
+	set_version_text()
+
+func set_version_text() -> void:
+	var version: String = ProjectSettings.get_setting("application/config/version") as String
+	var how_many_zeroes: int = version.count("0")
+
+	if how_many_zeroes == 1:
+		version_text.text = "v" + version.trim_suffix(".0")
+	elif how_many_zeroes > 1:
+		version_text.text = "v" + version.trim_suffix(".0.0")
+	else:
+		version_text.text = "v" + version
 
 func _on_play_pressed() -> void:
 	Global.main.stop_menu_theme()
